5e46b05b08b2ef9cb9c5e1a469936cea00c08be4
elena <boliasova64@mail.ru>
2022-03-20 20:25:46 +0900

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Write("Введите число => ");
+string strInput = Console.ReadLine();
+int[] arr = new int[strInput.Length];
+for (int i = 0; i < strInput.Length; i++)
+{
+    arr[i] = Convert.ToInt32(strInput.Substring(i, 1));
+}
+int Summ()
+{
+    int sum = 0;
+    for (int a = 0; a < arr.Length; a++)
+    {
+        sum = sum + arr[a];
+    }
+    return sum;
+}
+Console.WriteLine(Summ());
+
